551f75d4e1673031a5999c4dbebbe121de74f5e3
Xayzo <100010063+Xayzo@users.noreply.github.com>
2022-02-22 00:21:37 +0700

    Create mplayer.py
    To see the music player commands

---
+++ GabiBraunRobot/modules/mplayer.py
@@ -0,0 +1,20 @@
+__mod_name__ = "Music Player"
+
+__help__ = """
+*❂ Commands for admin only:*
+• `/userbotjoin` - For inviting the assistant to your groups
+• `/userbotleave` - Use this if you want the assistant leaving your groups
+• `/pause` - To pause the playback
+• `/vpause` - To pause video stream
+• `/resume` - To resuming the playback You've paused.
+• `/vresume` - To resuming video stream
+• `/skip` - To skipping the player
+• `/vskip` - To skipping the video stream
+• `/end` - For end the playback
+• `/vend` - For end the video stream
+• `/musicplayer` <on/off> - Toggle for turn ON or turn OFF the musicplayer
+
+*❂ Command for all members:*
+•  `/dplay` <query> - Playing music via YouTube
+•  `/vplay` <query or reply audio> - Playing video from YouTube
+"""
